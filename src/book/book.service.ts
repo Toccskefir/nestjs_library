4b71e7181320d9de14854624e6303ad441e6d460
Tóth Kevin <tothkevin0716@gmail.com>
2024-03-04 09:10:45 +0100

    Added initial data

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Book } from './book.model';
 
 @Injectable()
 export class BookService {
+  private readonly books: Book[] = [
+    {
+      id: '1',
+      title: 'Harry Potter',
+      author: 'J.K. Rowling',
+      publishYear: 1997,
+    },
+    {
+      id: '2',
+      title: 'Egri csillaagok',
+      author: 'Gárdonyi Géza',
+      publishYear: 1901,
+    },
+    {
+      id: '3',
+      title: 'The Great Gatsby',
+      author: 'F. Scott Fitzgerald',
+      publishYear: 1925,
+    },
+  ];
   getHello(): string {
     return 'Hello World!';
   }
