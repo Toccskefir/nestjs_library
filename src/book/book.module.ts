2859aff6334e1423516afc6878744f003c4d1295
Tóth Kevin <tothkevin0716@gmail.com>
2024-03-04 09:43:09 +0100

    Added validation pipe to book module

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [],
   controllers: [BookController],
-  providers: [BookService],
+  providers: [
+    BookService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({ transform: true, whitelist: true }),
+    },
+  ],
 })
 export class BookModule {}
